bb913f42d3a9c2c291d687841680c03d5160d35b
Pankaj Rathore <65541151+olive-green@users.noreply.github.com>
2022-06-28 02:02:54 +0530

    Time: 7 ms (85.25%), Space: 54.3 MB (45.35%) - LeetHub

--- 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.java
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.java
@@ -7,20 +7,25 @@ class Solution {
         //ex- 82734-11111=71623, 71623-11111=60512,60512-10111=50401
         
         
-        int i=1,j=n.length()-1;
-        int ans=Character.getNumericValue(n.charAt(0));
-        while(i<=j){
+//         int i=1,j=n.length()-1;
+//         int ans=Character.getNumericValue(n.charAt(0));
+//         while(i<=j){
             
-            if(Character.getNumericValue(n.charAt(i))>ans){
-                ans=Character.getNumericValue(n.charAt(i));
-            }
-            if(Character.getNumericValue(n.charAt(j))>ans){
-                ans=Character.getNumericValue(n.charAt(j));
-            }
-            i++;j--;
-        }
-        return ans;
+//             if(Character.getNumericValue(n.charAt(i))>ans){
+//                 ans=Character.getNumericValue(n.charAt(i));
+//             }
+//             if(Character.getNumericValue(n.charAt(j))>ans){
+//                 ans=Character.getNumericValue(n.charAt(j));
+//             }
+//             i++;j--;
+//         }
+//         return ans;
         
+        // another sol
+         int res = 0;
+        for (int i = 0; i < n.length(); ++i)
+            res = Math.max(res, n.charAt(i) - '0');
+        return res;
         
     }
 }
