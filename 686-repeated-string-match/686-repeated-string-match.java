2e2bd8a948af78ea05f25b19abce0b7005078ae2
Pankaj Rathore <65541151+olive-green@users.noreply.github.com>
2022-07-03 01:29:21 +0530

    Time: 2346 ms (5.09%), Space: 117.4 MB (10.39%) - LeetHub

--- 686-repeated-string-match/686-repeated-string-match.java
+++ 686-repeated-string-match/686-repeated-string-match.java
@@ -1,8 +1,38 @@
 class Solution {
+    public boolean isBsubstring(String a,String b){
+        int n=a.length();
+        int m=b.length();
+        
+        for(int i=0;i<=n-m;i++){
+            int j;
+            for(j=0;j<m;j++){
+                if(a.charAt(i+j)!=b.charAt(j)){
+                    break;
+                }
+            }
+            if(j==m)
+                return true;
+        }
+        return false;
+    }
     public int repeatedStringMatch(String a, String b) {
-        String as = a;
-        for (int rep = 1; rep <= b.length() / a.length() + 2; rep++, as += a)
-            if (as.indexOf(b) != -1) return rep;
+        // String as = a;
+        // for (int rep = 1; rep <= b.length() / a.length() + 2; rep++, as += a)
+        //     if (as.indexOf(b) != -1) return rep;
+        // return -1;
+        
+        String temp=a;
+        
+        int ans=1;
+        while(a.length()<b.length()){
+            a+=temp;
+            ans++;
+        }
+        if(isBsubstring(a,b))
+            return ans;
+        
+        if(isBsubstring(a+temp,b)) return ans+1;
+        
         return -1;
     }
 }
