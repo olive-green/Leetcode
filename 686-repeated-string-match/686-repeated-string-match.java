f5880ef9979505d9fa365054aa36e4f4da2d37c9
Pankaj Rathore <65541151+olive-green@users.noreply.github.com>
2022-07-03 01:43:24 +0530

    Time: 434 ms (25.00%), Space: 113.3 MB (28.36%) - LeetHub

--- 686-repeated-string-match/686-repeated-string-match.java
+++ 686-repeated-string-match/686-repeated-string-match.java
@@ -1,38 +1,49 @@
 class Solution {
-    public boolean isBsubstring(String a,String b){
-        int n=a.length();
-        int m=b.length();
+//     public boolean isBsubstring(String a,String b){
+//         int n=a.length();
+//         int m=b.length();
         
-        for(int i=0;i<=n-m;i++){
-            int j;
-            for(j=0;j<m;j++){
-                if(a.charAt(i+j)!=b.charAt(j)){
-                    break;
-                }
-            }
-            if(j==m)
-                return true;
-        }
-        return false;
-    }
+//         for(int i=0;i<=n-m;i++){
+//             int j;
+//             for(j=0;j<m;j++){
+//                 if(a.charAt(i+j)!=b.charAt(j)){
+//                     break;
+//                 }
+//             }
+//             if(j==m)
+//                 return true;
+//         }
+//         return false;
+//     }
     public int repeatedStringMatch(String a, String b) {
-        // String as = a;
-        // for (int rep = 1; rep <= b.length() / a.length() + 2; rep++, as += a)
-        //     if (as.indexOf(b) != -1) return rep;
-        // return -1;
-        
-        String temp=a;
-        
-        int ans=1;
-        while(a.length()<b.length()){
-            a+=temp;
-            ans++;
+        String newStrA = a;
+        int count=1;
+        //now we have to find how many max no. of time a have to repeat to get substring of b
+        int repeat=b.length()/a.length() +2; //2 is added because one time for first character and one time for last character like in case z xyzxyzxyz
+        for(int i=0;i<repeat;i++){
+            if(newStrA.contains(b))
+                return count;
+            //else repeat a
+            newStrA+=a;
+            count++;
         }
-        if(isBsubstring(a,b))
-            return ans;
-        
-        if(isBsubstring(a+temp,b)) return ans+1;
-        
         return -1;
+    
+        
+        
+        
+//         String temp=a;
+        
+//         int ans=1;
+//         while(a.length()<b.length()){
+//             a+=temp;
+//             ans++;
+//         }
+//         if(isBsubstring(a,b))
+//             return ans;
+        
+//         if(isBsubstring(a+temp,b)) return ans+1;
+        
+//         return -1;
     }
 }
