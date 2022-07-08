1d2e72b6f137974c7957b3c92d82939d3d9c0d34
Pankaj Rathore <65541151+olive-green@users.noreply.github.com>
2022-07-08 12:32:56 +0530

    Time: 1 ms (87.94%), Space: 43.1 MB (42.01%) - LeetHub

--- 50-powx-n/50-powx-n.java
+++ 50-powx-n/50-powx-n.java
@@ -17,42 +17,38 @@ class Solution {
         // like 1.00 n=2^31
         
         
-        // 2.logarthmic solution
-//         if(n==0) return 1;
-//         if(n==1) return x;
-//         double xpbn2;
-//         if(n<0){
-//              xpbn2=myPow(1/x,-n/2);
-//              if(-n%2==0)
-//                  return xpbn2*xpbn2;
-//             else 
-//                 return xpbn2*xpbn2*(1/x);
-//         }
+        // 2.logarthmic recursive solution
+        if(n==0) return 1;
+        if(n==1) return x;
         
-//         xpbn2=myPow(x,n/2);
-//         if(n%2==0)
-//             return xpbn2*xpbn2;
-//         return xpbn2*xpbn2*x;
-            
-        double ans=1;
-        long tempn=n;
-        if(n<0) tempn=-(tempn);
-        while(tempn>0){
-           if(tempn%2==0){
-              x=x*x;
-              tempn=tempn/2; 
-           }
-            else{
-                ans=ans*x;
-                tempn=tempn-1;
-            }
-        }
+        double res=myPow(x*x,Math.abs(n/2));
+        if(n%2!=0)
+            res=res*x;
         if(n<0)
-            return  1/ans;
-        return ans;
+            return 1/res;
+        return res;
+            
+        // OR
+        //Logarthmic iterative solution
+//         double ans=1;
+//         long tempn=n;
+//         if(n<0) tempn=-(tempn);
+//         while(tempn>0){
+//            if(tempn%2==0){
+//               x=x*x;
+//               tempn=tempn/2; 
+//            }
+//             else{
+//                 ans=ans*x;
+//                 tempn=tempn-1;
+//             }
+//         }
+//         if(n<0)
+//             return  1/ans;
+//         return ans;
         
-        
-        
+// //         OR
+        //logarthmic bitwise solution
 //         if(n < 0){
 //             n = -n;
 //             x = 1 / x;
