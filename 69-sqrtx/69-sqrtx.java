fd22689b48fb228be8a94ba7685e01578b63ea00
Pankaj Rathore <65541151+olive-green@users.noreply.github.com>
2022-07-08 13:01:48 +0530

    Time: 2 ms (82.70%), Space: 41 MB (71.95%) - LeetHub

--- 69-sqrtx/69-sqrtx.java
+++ 69-sqrtx/69-sqrtx.java
@@ -1,18 +1,37 @@
 class Solution {
     public int mySqrt(int x) {
-        //linear time
-        if(x==0)
-            return 0;
-        if(x<4)
-            return 1;
-        double ans=0;
-        for(double i=2;i<=x/2;i++){
-            if(i*i==x)
-                return (int)i;
-            if(i*i>x)
-                break;
-            ans=i;
+        //linear time or logarthmic time
+        // if(x==0)
+        //     return 0;
+        // if(x<4)
+        //     return 1;
+        // double ans=0;
+        // for(double i=2;i<=x/2;i++){
+        //     if(i*i==x)
+        //         return (int)i;
+        //     if(i*i>x)
+        //         break;
+        //     ans=i;
+        // }
+        // return (int)ans;
+        
+        //logarthmic solution
+        //binary search we find m*m==x
+        long s=0,e=Integer.MAX_VALUE;
+        int ans=0;
+        while(s<=e){
+            long mid=s+(e-s)/2;
+            
+            
+            if(mid*mid<=x){
+                ans=(int)mid;
+                 s=mid+1;
+            }
+               
+            else
+                e=mid-1;
+            
         }
-        return (int)ans;
+        return ans;
     }
 }
