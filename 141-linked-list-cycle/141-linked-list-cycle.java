3c62665677887beb7585f9137f0a754e39acb9dd
Pankaj Rathore <65541151+olive-green@users.noreply.github.com>
2022-07-14 20:11:10 +0530

    Time: 1 ms (44.75%), Space: 46.4 MB (25.78%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.java
+++ 141-linked-list-cycle/141-linked-list-cycle.java
@@ -13,19 +13,38 @@ public class Solution {
     public boolean hasCycle(ListNode head) {
         
         // 1st approach 
-        ListNode temp=head;
+        // ListNode temp=head;
         // HashMap<ListNode,Integer> mp=new HashMap<>();
-        HashSet<ListNode> set=new HashSet<>();
-        while(temp!=null){
-            // if(!mp.contains(temp)){
-            //     mp.put(temp,1);
-            // }
-            if(!set.contains(temp))
-                set.add(temp);
-            else
+        // HashSet<ListNode> set=new HashSet<>();
+        // while(temp!=null){
+        //     // if(!mp.contains(temp)){
+        //     //     mp.put(temp,1);
+        //     // }
+        //     if(!set.contains(temp))
+        //         set.add(temp);
+        //     else
+        //         return true;
+        //     temp=temp.next;
+        // }
+        // return false;
+        
+        
+        // 2nd approach
+        //using floyad cycle detection algo , in which we use fast and slow pointer when both are same return true;
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            
+            if(fast==slow)
+            {
+                System.out.println(fast.val);
                 return true;
-            temp=temp.next;
+            }
+                
         }
         return false;
+        
     }
 }
