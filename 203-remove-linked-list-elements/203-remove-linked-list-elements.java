5dcc479accddd5db8c09c77b1096af145a6aecfc
Pankaj Rathore <65541151+olive-green@users.noreply.github.com>
2022-07-16 02:21:36 +0530

    Time: 1 ms (98.99%), Space: 49.8 MB (5.32%) - LeetHub

--- 203-remove-linked-list-elements/203-remove-linked-list-elements.java
+++ 203-remove-linked-list-elements/203-remove-linked-list-elements.java
@@ -10,20 +10,38 @@
  */
 class Solution {
     public ListNode removeElements(ListNode head, int val) {
-        ListNode temp=head;
-        ListNode prev=temp;
-        while(temp!=null){
-            while(temp!=null && temp.val==val){
-                ListNode free=temp;
-                temp=temp.next;
-                free=null;
-                prev.next=temp;
-            }   
-            prev=temp;
-            // if(prev!=null) System.out.println(prev.val);
-            temp= (temp!=null)? temp.next : null;
+        // ListNode temp=head;
+        // ListNode prev=temp;
+        // while(temp!=null){
+        //     while(temp!=null && temp.val==val){
+        //         ListNode free=temp;
+        //         temp=temp.next;
+        //         free=null;
+        //         prev.next=temp;
+        //     }   
+        //     prev=temp;
+        //     // if(prev!=null) System.out.println(prev.val);
+        //     temp= (temp!=null)? temp.next : null;
+        // }
+        // if(head!=null && head.val==val) return head.next;
+        // return head;
+        
+        // 2nd approach
+        //above is good but in better way
+        ListNode fakeHead=new ListNode(-1);
+        fakeHead.next=head;
+        ListNode curr=head;
+        ListNode prev=fakeHead;
+        
+        while(curr!=null){
+            if(curr.val==val){
+                prev.next=curr.next;
+            }
+            else
+                prev=prev.next;
+            curr=curr.next;
+            
         }
-        if(head!=null && head.val==val) return head.next;
-        return head;
+        return fakeHead.next;
     }
 }
