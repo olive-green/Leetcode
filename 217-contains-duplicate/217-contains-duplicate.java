29ead0a4a4bdd42ec8d501435d5298caaf508f7c
Pankaj Rathore <65541151+olive-green@users.noreply.github.com>
2022-07-16 17:41:25 +0530

    Time: 26 ms (19.90%), Space: 72.3 MB (11.15%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        //we can do it by using xor do check it soulution
+        HashMap<Integer,Integer> mp=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            mp.put(nums[i],mp.getOrDefault(nums[i],0)+1);
+            if(mp.get(nums[i]) >1)
+                return true;
+            
+        }
+      
+        return false;
+    }
+}
