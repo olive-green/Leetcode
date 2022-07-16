c4be99bfda792884579b9000559c483e06dbd7d0
Pankaj Rathore <65541151+olive-green@users.noreply.github.com>
2022-07-16 19:31:17 +0530

    Time: 1 ms (70.27%), Space: 42.3 MB (50.61%) - LeetHub

---
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.java
@@ -0,0 +1,65 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int size(ListNode head){
+        ListNode temp=head;
+        int count=0;
+        while(temp!=null){
+            count++;
+            temp=temp.next;
+        }
+        return count;
+    }
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+//         if(head.next==null)
+//             return head=null;
+//         int siz=size(head);
+//         if(n==siz){
+//             head=head.next;
+//             return head;
+//         }
+        
+//         ListNode slow=head;
+//         ListNode fast=head;
+//         for(int i=1;i<n;i++){
+//             fast=fast.next;
+//         }
+//         while(fast.next!=null && fast.next.next!=null){
+//             fast=fast.next;
+//             slow=slow.next;
+//         }
+//         //NOW SLOW IS POINTING PREV NODE OF TARGET NODE
+//         //SO WE HAVE TO JUST POINT THIS NODE TO TARGET.NEXT
+//         slow.next=slow.next.next;
+//         return head;
+      
+        if(head.next==null)
+            return head=null;
+        //first we move A pointer to k then move both A and B till A!=null after that B is at kth position
+        ListNode fakeHead=new ListNode(-1);
+        ListNode a=fakeHead;
+        ListNode b=fakeHead;
+        fakeHead.next=head;
+        
+        for(int i=0;i<=n;i++){
+            a=a.next;
+        }
+        
+        while(a!=null){
+            a=a.next;
+            b=b.next;
+        }
+        //now b points to one previous of kth node
+        b.next=b.next.next;
+        return fakeHead.next;
+        
+    }
+}
