9d05ec39adaec755128a711723c2a05dcf956932
Pankaj Rathore <65541151+olive-green@users.noreply.github.com>
2022-07-16 17:43:30 +0530

    Time: 19 ms (35.08%), Space: 69.2 MB (54.69%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.java
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -3,9 +3,11 @@ class Solution {
         //we can do it by using xor do check it soulution
         HashMap<Integer,Integer> mp=new HashMap<>();
         for(int i=0;i<nums.length;i++){
-            mp.put(nums[i],mp.getOrDefault(nums[i],0)+1);
-            if(mp.get(nums[i]) >1)
+            if(mp.containsKey(nums[i]))
                 return true;
+            mp.put(nums[i],mp.getOrDefault(nums[i],0)+1);
+            // if(mp.get(nums[i]) >1)
+            //     return true;
             
         }
       
