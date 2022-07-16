03748dd6173d62a069a29a059d33f31456b16ffa
Pankaj Rathore <65541151+olive-green@users.noreply.github.com>
2022-07-16 18:47:42 +0530

    Time: 5 ms (97.28%), Space: 65.1 MB (74.73%) - LeetHub

--- 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.java
+++ 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.java
@@ -1,12 +1,28 @@
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
-        HashSet<Integer> set=new HashSet<>();
+        // HashSet<Integer> set=new HashSet<>();
+        // List<Integer> ans=new ArrayList<>();
+        // for(int i=0;i<nums.length;i++){
+        //     set.add(nums[i]);
+        // }
+        // for(int i=1;i<=nums.length;i++){
+        //     if(!set.contains(i)){
+        //         ans.add(i);
+        //     }
+        // }
+        // return ans;
+        
+        
+        // 2.Instead of using hashset we can use boolean array
+        
+        boolean[] check=new boolean[nums.length+1];
         List<Integer> ans=new ArrayList<>();
         for(int i=0;i<nums.length;i++){
-            set.add(nums[i]);
+            // set.add(nums[i]);
+            check[nums[i]]=true;
         }
         for(int i=1;i<=nums.length;i++){
-            if(!set.contains(i)){
+            if(!check[i]){
                 ans.add(i);
             }
         }
