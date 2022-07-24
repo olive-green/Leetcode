62df033fed9f11dab4e702ad6548d0a1099f5646
Pankaj Rathore <65541151+olive-green@users.noreply.github.com>
2022-07-24 20:00:29 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (75.00%) - LeetHub

--- 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.java
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.java
@@ -1,10 +1,15 @@
 class Solution {
     public char repeatedCharacter(String s) {
-        HashSet<Character> freq=new HashSet<>();
+        // HashSet<Character> freq=new HashSet<>();
+        boolean visited[]=new boolean[26];
         for(int i=0;i<s.length();i++){
-            if(freq.contains(s.charAt(i)))
+            // if(freq.contains(s.charAt(i)))
+            //     return s.charAt(i);
+            // freq.add(s.charAt(i));
+            if(visited[s.charAt(i) - 'a']==true){
                 return s.charAt(i);
-            freq.add(s.charAt(i));
+            }
+            visited[s.charAt(i) - 'a']=true;
         }
         return s.charAt(s.length()-1);
     }
