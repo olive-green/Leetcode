e36cc3bab5eabff72a19d9eb6879e429628f29d5
Pankaj Rathore <65541151+olive-green@users.noreply.github.com>
2022-07-27 18:00:31 +0530

    Time: 15 ms (25.98%), Space: 43.7 MB (84.86%) - LeetHub

--- 784-letter-case-permutation/784-letter-case-permutation.java
+++ 784-letter-case-permutation/784-letter-case-permutation.java
@@ -1,24 +1,32 @@
 class Solution {
-    public List<String> letterCasePermutation(String s) {
-        List<String>ans=new ArrayList<>();
-        helper(ans,0,s.toCharArray());
-        return ans;
-    }
-    public  void helper(List<String>ans,int i,char []s)
-    {
-        if(i==s.length)
-            ans.add(new String(s));
-        else
-        {
-            if(Character.isLetter(s[i]))
-            {
-                s[i]=Character.toUpperCase(s[i]);
-                helper(ans,i+1,s);
-                s[i]=Character.toLowerCase(s[i]);
-                helper(ans,i+1,s);
+    public void pmt(int i,String s,List<String> ans,String asf){
+        if(i==s.length()){
+            ans.add(asf);
+            return;
+        }        
+            if(!Character.isDigit(s.charAt(i))){
+                //lowercase char added
+                String left=s.substring(0,i);
+                // System.out.println(left);
+                String right=s.substring(i+1);
+                s=left+Character.toLowerCase(s.charAt(i))+ right;
+                pmt(i+1,s,ans,asf+s.charAt(i));
+                //uppercase char added
+                // now to make uppercase letter as we know we cannot make change in s so we have to make new string in which ith char is uppercase
+                
+                s=left+Character.toUpperCase(s.charAt(i))+right;
+                pmt(i+1,s,ans,asf+s.charAt(i));
             }
             else
-                helper(ans,i+1,s);
-        }
+            {
+                
+                pmt(i+1,s,ans,asf+s.charAt(i));
+            }
+        
+    }
+    public List<String> letterCasePermutation(String s) {
+        List<String> ans=new ArrayList<>();
+        pmt(0,s,ans,"");
+        return ans;
     }
 }
