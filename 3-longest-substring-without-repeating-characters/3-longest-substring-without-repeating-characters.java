a40b47818845262ed31ab60d2d9831b61a1ee119
Pankaj Rathore <65541151+olive-green@users.noreply.github.com>
2022-08-09 00:44:23 +0530

    Time: 30 ms (20.08%), Space: 46.2 MB (29.85%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
@@ -1,37 +1,69 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-         if (s.length()==0) return 0;
-        HashMap<Character, Integer> map = new HashMap<>();
-        int ans=0;
+        //sliding window
         int i=0,j=0;
+        HashMap<Character,Integer> mp=new HashMap<>();
+        // for(int k=0;k<s.length();k++){
+        //     mp.put(s.charAt(k),mp.getOrDefault(s.charAt(k),0)+1);
+        // }
+        int ans=0;
         while(j<s.length()){
-            char x=s.charAt(j);
-            if(!map.containsKey(x)){
-                map.put(x,1);
+             mp.put(s.charAt(j),mp.getOrDefault(s.charAt(j),0)+1);
+            if(mp.size()>j-i+1){//this condition never hits so you can remove it
+                j++;
             }
-            else{
-                map.put(x,map.get(x)+1);
-            }
-            if(map.size()==j-i+1){
+            if(mp.size()==(j-i+1)){
+                //found valid ans
                 ans=Math.max(ans,j-i+1);
                 j++;
             }
-            else { //map.size()<j-i+1
-                while(map.size()<j-i+1){
-                    char t=s.charAt(i);
-                    if(map.containsKey(t)){
-                        if(map.get(t)==1)
-                            map.remove(t);
-                        else{
-                            map.put(t,map.get(t)-1);
-                        }
-                    }
+            else if(mp.size()<j-i+1){
+                
+                while(mp.size()<j-i+1){
+                    char ch=s.charAt(i);
+                    mp.put(ch,mp.get(ch)-1);
+                    if(mp.get(ch)==0)
+                        mp.remove(ch);
                     i++;
                 }
                 j++;
             }
         }
+        System.out.println(mp);
         return ans;
+        
+//          if (s.length()==0) return 0;
+//         HashMap<Character, Integer> map = new HashMap<>();
+//         int ans=0;
+//         int i=0,j=0;
+//         while(j<s.length()){
+//             char x=s.charAt(j);
+//             if(!map.containsKey(x)){
+//                 map.put(x,1);
+//             }
+//             else{
+//                 map.put(x,map.get(x)+1);
+//             }
+//             if(map.size()==j-i+1){
+//                 ans=Math.max(ans,j-i+1);
+//                 j++;
+//             }
+//             else { //map.size()<j-i+1
+//                 while(map.size()<j-i+1){
+//                     char t=s.charAt(i);
+//                     if(map.containsKey(t)){
+//                         if(map.get(t)==1)
+//                             map.remove(t);
+//                         else{
+//                             map.put(t,map.get(t)-1);
+//                         }
+//                     }
+//                     i++;
+//                 }
+//                 j++;
+//             }
+//         }
+//         return ans;
         
     }
 }
