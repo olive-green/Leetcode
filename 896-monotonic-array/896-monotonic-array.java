9f2aeb290bd70e74ebb2b5ca943f3281e079ec20
Pankaj Rathore <65541151+olive-green@users.noreply.github.com>
2022-08-13 13:32:45 +0530

    Time: 4 ms (48.27%), Space: 93 MB (46.92%) - LeetHub

--- 896-monotonic-array/896-monotonic-array.java
+++ 896-monotonic-array/896-monotonic-array.java
@@ -5,12 +5,11 @@ class Solution {
             if(nums[i]>nums[i+1]){
                 inc=false;
             }
-        }
-        for(int i=0;i<nums.length-1;i++){
             if(nums[i]<nums[i+1]){
                 dec=false;
             }
         }
+        
         if(inc || dec)
             return true;
         return false;
