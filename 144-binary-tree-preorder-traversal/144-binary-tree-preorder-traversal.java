ae3e804c0cf1c1a6f663a6cc06d37709f189129d
Pankaj Rathore <65541151+olive-green@users.noreply.github.com>
2022-08-28 15:06:29 +0530

    Time: 1 ms (50.74%), Space: 42.3 MB (50.90%) - LeetHub

--- 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.java
+++ 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.java
@@ -20,17 +20,30 @@ class Solution {
         List<Integer> ans=new ArrayList<>();
         
         Stack<TreeNode> st=new Stack();
-        st.add(root);
-        while(!st.isEmpty()){
-            //pop top
-            TreeNode top=st.pop();
-            ans.add(top.val);
-            //now first push right child then left for preorder bcz to process left child first
+        // st.add(root);
+//         while(!st.isEmpty()){
+//             //pop top
+//             TreeNode top=st.pop();
+//             ans.add(top.val);
+//             //now first push right child then left for preorder bcz to process left child first
             
-            if(top.right!=null)
-                st.push(top.right);
-            if(top.left!=null)
-                st.push(top.left);
+//             if(top.right!=null)
+//                 st.push(top.right);
+//             if(top.left!=null)
+//                 st.push(top.left);
+//         }
+        
+        //another approach
+        //in this approach we uses stack to store right child only we doesn't store left child
+        //if left child finishes then start popping out right child
+        while(root!=null){
+            ans.add(root.val);
+            if(root.right!=null)
+              st.add(root.right);
+            root=root.left;
+            
+            if(root==null && !st.isEmpty())
+                root=st.pop();           
         }
         return ans;
     }
