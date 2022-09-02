6f6cc74ed83c0af7d6d4d2fbc3832c3c912e9bfd
Pankaj Rathore <65541151+olive-green@users.noreply.github.com>
2022-09-03 00:01:19 +0530

    Time: 7 ms (72.98%), Space: 42.4 MB (86.33%) - LeetHub

---
+++ 139-word-break/139-word-break.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> dict) {
+        boolean[] f = new boolean[s.length() + 1];
+        
+        f[0] = true;
+        
+        
+
+        for(int i = 1; i <= s.length(); i++){
+            for(String str: dict){
+                if(str.length() <= i){
+                    if(f[i - str.length()]){
+                        if(s.substring(i-str.length(), i).equals(str)){
+                            f[i] = true;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        return f[s.length()];
+    }
+}
