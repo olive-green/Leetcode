65e9c0f80b82c5c1be6d1d71b07fe48ba79324b3
Pankaj Rathore <65541151+olive-green@users.noreply.github.com>
2022-09-03 04:10:36 +0530

    Time: 1 ms (97.62%), Space: 41.9 MB (98.23%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,24 +1,33 @@
 class Solution {
     public int maxProfit(int[] p) {
-//         int i = 0, buy, sell, profit = 0, N = prices.length - 1;
-//         while (i < N) {
-//             while (i < N && prices[i + 1] <= prices[i]) i++;
-//             buy = prices[i];
 
-//             while (i < N && prices[i + 1] > prices[i]) i++;
-//             sell = prices[i];
-
-//             profit += sell - buy;
-//         }
-//         return profit;
+        //you can think of buying on one day and selling on adjacent day then you get the ans
+        // int ans=0;
+        // for(int i=0;i<p.length-1;i++){
+        //     if(p[i+1]-p[i]>0){
+        //         ans+=p[i+1]-p[i];
+        //     }
+        // }
+        // return ans;
         
-        //you can think of buying on ond day and selling on adjacent day then you get the ans
+        // One more way to solve the problem is to think of the situation when we buy the stocks in the beginning of the upstreak in the stock graph and sell it at the highest point of that upstreak line of the graph. We just have to calculate the sum of all the upstreaks that are present in the graph.
+        int buyDate=0;
+        int sellDate=0;
+        
         int ans=0;
-        for(int i=0;i<p.length-1;i++){
-            if(p[i+1]-p[i]>0){
-                ans+=p[i+1]-p[i];
+        for(int i=1;i<p.length;i++){
+            if(p[i]>=p[i-1]){
+                sellDate++;
+            }
+            else{
+                ans+=p[sellDate]-p[buyDate];
+                sellDate=buyDate=i;
             }
         }
+        //incase when array is increasing order
+        ans+=p[sellDate]-p[buyDate]; //sellDate is last ele and buydate is first element
         return ans;
+        
+        
     }
 }
