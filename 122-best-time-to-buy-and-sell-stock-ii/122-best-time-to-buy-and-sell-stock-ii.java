86b09a598356387731dc7dd3785cf28e5640a11f
Pankaj Rathore <65541151+olive-green@users.noreply.github.com>
2022-09-03 03:49:03 +0530

    Time: 1 ms (97.62%), Space: 44.1 MB (68.18%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,15 +1,24 @@
 class Solution {
-    public int maxProfit(int[] prices) {
-        int i = 0, buy, sell, profit = 0, N = prices.length - 1;
-        while (i < N) {
-            while (i < N && prices[i + 1] <= prices[i]) i++;
-            buy = prices[i];
+    public int maxProfit(int[] p) {
+//         int i = 0, buy, sell, profit = 0, N = prices.length - 1;
+//         while (i < N) {
+//             while (i < N && prices[i + 1] <= prices[i]) i++;
+//             buy = prices[i];
 
-            while (i < N && prices[i + 1] > prices[i]) i++;
-            sell = prices[i];
+//             while (i < N && prices[i + 1] > prices[i]) i++;
+//             sell = prices[i];
 
-            profit += sell - buy;
+//             profit += sell - buy;
+//         }
+//         return profit;
+        
+        //you can think of buying on ond day and selling on adjacent day then you get the ans
+        int ans=0;
+        for(int i=0;i<p.length-1;i++){
+            if(p[i+1]-p[i]>0){
+                ans+=p[i+1]-p[i];
+            }
         }
-        return profit;
+        return ans;
     }
 }
