9abde2bd3d650266779f27a4cd51f3f1d9c31641
Pankaj Rathore <65541151+olive-green@users.noreply.github.com>
2023-01-02 14:32:46 +0530

    Time: 5 ms (24.37%), Space: 6 MB (70.23%) - LeetHub

--- 520-detect-capital/520-detect-capital.cpp
+++ 520-detect-capital/520-detect-capital.cpp
@@ -19,13 +19,38 @@ public:
         return true;
     }
     bool detectCapitalUse(string word) {
-        if(upperCase(word))
+        // if(upperCase(word))
+        //     return true;
+        // else if(lowerCase(word))
+        //     return true;
+        // else if(isupper(word[0]) && lowerCase(word.substr(1)))
+        //     return true;
+        // else 
+        //     return false;
+        
+//         Approach:
+
+//     Initialize count=0
+//     if count equals 0 means all are small chars
+//     if count equals size of word then all are capital
+//     if count equals 1 then check is it the first char which is capital  ex- "ffffffffffffffffffffF"
+//     if none of the conditions are true then return false
+        int count=0;
+        if(word.size()==1)
             return true;
-        else if(lowerCase(word))
+        
+        for(int i=0; i<word.size(); i++)
+            if(isupper(word[i]))
+                count++;
+        
+        if(count==1 && isupper(word[0]))
             return true;
-        else if(isupper(word[0]) && lowerCase(word.substr(1)))
+        if(count==0 || count==word.size())
             return true;
-        else 
+        else
             return false;
+
+
     }
+    
 };
