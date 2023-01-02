3a75439df1acbebbd4d7ca22128c911aa1a6f426
Pankaj Rathore <65541151+olive-green@users.noreply.github.com>
2023-01-02 14:33:18 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (24.57%) - LeetHub

--- 520-detect-capital/520-detect-capital.cpp
+++ 520-detect-capital/520-detect-capital.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    bool upperCase(string s){
-        for(int i=0;i<s.size();i++){
-            if(s[i]>=65 && s[i]<=90)
-                continue;
-            else
-                return false;
-        }
-        return true;
-    }
-    bool lowerCase(string s){
-        for(int i=0;i<s.size();i++){
-            if(s[i]>=97 && s[i]<=122)
-                continue;
-            else
-                return false;
-        }
-        return true;
-    }
+    // bool upperCase(string s){
+    //     for(int i=0;i<s.size();i++){
+    //         if(s[i]>=65 && s[i]<=90)
+    //             continue;
+    //         else
+    //             return false;
+    //     }
+    //     return true;
+    // }
+    // bool lowerCase(string s){
+    //     for(int i=0;i<s.size();i++){
+    //         if(s[i]>=97 && s[i]<=122)
+    //             continue;
+    //         else
+    //             return false;
+    //     }
+    //     return true;
+    // }
     bool detectCapitalUse(string word) {
         // if(upperCase(word))
         //     return true;
